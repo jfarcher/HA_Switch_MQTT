15bca8873be13a01a6f7bbb7c83b414757f019ff
jfarcher <jf_archer@yahoo.com>
2013-11-12 01:34:28 +0000

    Committing the actual code this time not some earlier attempt.

--- mqttlightcontrol_switchside.ino
+++ mqttlightcontrol_switchside.ino
@@ -1,28 +1,24 @@
-/* 
-Toggle switch to send relevant mqtt message to turn on/off device.
-
-Code originated in the Debounce Example and has been hacked to bits
+/*
+ Basic MQTT example with Authentication
  
- created 21 November 2006
- by David A. Mellis
- modified 30 Aug 2011
- by Limor Fried
- modified 28 Dec 2012
- by Mike Walters
- 
- This example code is in the public domain.
- http://www.arduino.cc/en/Tutorial/Debounce
- */
+  - connects to an MQTT server, providing username
+    and password
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic"
+*/
 
-#include <PubSubClient.h>
-#include <OneWire.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include <PubSubClient.h>
+#include <OneWire.h>
+
+
+
+// Update these with values suitable for your network.
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+OneWire ds(2);
 
 #define MQTT_SERVER "192.168.1.3"
-
-byte mac[]= { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x03 };
-OneWire ds(2);
 IPAddress ip(192,168,1, 178);
 IPAddress gateway(192,168,1, 1);
 IPAddress subnet(255, 255, 255, 0);
@@ -34,7 +30,9 @@ const int ledPin =  13;      // the number of the LED pin
 
 // Variables will change:
 int ledState = HIGH;         // the current state of the output pin
-int buttonState;             // the current reading from the input pin
+int buttonState;
+char* swState;
+// the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 
 // the following variables are long's because the time, measured in miliseconds,
@@ -42,19 +40,22 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-//callback for MQTT - not sure if this is required due to us not receiving
+
 void callback(char* topic, byte* payload, unsigned int length) {
- // handle message arrived
+  // handle message arrived
 }
 
-PubSubClient client(MQTT_SERVER,1883,callback);
-void setup() {
+EthernetClient ethClient;
+PubSubClient client(MQTT_SERVER, 1883, callback, ethClient);
+
+void setup()
+{
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
     Serial.begin(9600);
      byte i;
   byte dsAddress[8]; //ds on pin 8
-  
+ 
   Serial.begin(9600);
   delay( 500 );
   Serial.print ("Searching for DS18B20...");
@@ -85,17 +86,21 @@ void setup() {
     for(;;)
       ;
   }
-Serial.print("IP address: ");
+  Serial.print("IP address: ");
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
     // print the value of each byte of the IP address:
     Serial.print(Ethernet.localIP()[thisByte], DEC);
   }
   delay(10);
   Serial.println();
+if (client.connect("arduinoClient")) {      // connecting to MQTT server
+   Serial.println("Connected to MQTT");      // Milestone for connection debugging
+  }
 }
 
-void loop() {
-  // read the state of the switch into a local variable:
+void loop()
+{
+ // read the state of the switch into a local variable:
   int reading = digitalRead(buttonPin);
 
  
@@ -114,6 +119,14 @@ void loop() {
       if (buttonState == HIGH) {
       ledState = !ledState;
       Serial.println(ledState);
+      Serial.println("sending state");
+      if (ledState == 0){
+        swState = "2-40";
+      }
+      if (ledState == 1){
+        swState = "2-41";
+      }
+      client.publish("house/room1/1",swState);
     } 
   } 
   
@@ -132,3 +145,4 @@ void loop() {
   // it'll be the lastButtonState:
   lastButtonState = reading;
 }
+
